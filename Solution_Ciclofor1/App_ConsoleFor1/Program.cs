5d1b9b05adceee3892224e8250871647e7bf7312
studenti <studenti@LAB2-PC-18>
2018-01-22 09:19:12 +0100

    2

---
+++ Solution_Ciclofor1/App_ConsoleFor1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_ConsoleFor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine("Inserire un numero ");
+            a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("Lavoro TPSIT..");
+            }
+            Console.ReadLine();
+        }
+    }
+}
